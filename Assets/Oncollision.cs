afb78f56ab64419a013b633b6755746b491f7a14
starkey <astroo22@gmail.com>
2019-01-31 15:28:11 -0600

    Added two more victims
    Did things

---
+++ Assets/Oncollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oncollision : MonoBehaviour
+{
+    public AudioSource scream;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GetComponent<AudioSource>().playOnAwake = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)  //Plays Sound Whenever collision detected
+    {
+        if(collision.gameObject.tag =="Rock")
+        {
+            scream = GetComponent<AudioSource>();
+            scream.Play();
+        }
+        
+    }
+}
