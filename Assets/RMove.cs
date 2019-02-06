95d70c29bbfeda0a223446ece130829f492fab1e
starkey <astroo22@gmail.com>
2019-02-06 12:35:49 -0600

    Keeps crashing unity?
    unity got rkt

---
+++ Assets/RMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RMove : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private bool isup = false;
+    private float nextActionTime = 0.0f;
+    public float period = 3.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Vector3 Vict = transform.position;
+
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            if(!isup)
+            {
+                while(transform.position.y != 0)
+                {
+                    transform.position += Vector3.up * speed * Time.deltaTime;
+                }
+                isup = true;
+            }
+            else
+            {
+                while (transform.position.y != -5)
+                {
+                    transform.position += Vector3.down * speed * Time.deltaTime;
+                }
+                isup = false;
+            }
+            
+
+        }
+        transform.position += Vector3.down * speed * Time.deltaTime;
+    }
+}
